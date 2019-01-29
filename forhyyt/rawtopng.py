099d82d9d63ce96f8b1e2e4843dd68a789b951c7
root <root@xxxxx.xxxxx>
2019-01-29 22:22:59 +0100

    fpd

--- forhyyt/rawtopng.py
+++ forhyyt/rawtopng.py
@@ -3,10 +3,10 @@ from PIL import Image
 
 f = open("packetdump", "rb")  # reopen the file
 data=f.read()
-#for x in range(0,1024):
-x=570
 y=1070
-img = Image.frombuffer('L', (512+x,y), data)
-filename="test"+str(x)+".png"
+for x in range(0,1024):
+#x=570
+    img = Image.frombuffer('L', (512+x,y), data)
+    filename="test"+str(x)+".png"
     
-img.save(filename)
+    img.save(filename)
