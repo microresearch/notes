f2d51df1f6ee998762d8cd735b0f4c8aed188ac0
root <root@xxxxx.xxxxx>
2019-06-19 11:11:33 +0200

    randomwalk

--- oracles/randomwalk.py
+++ oracles/randomwalk.py
@@ -7,8 +7,8 @@
 # - if it is <100 we go left x metres, else go right (x metres based on abs from 100 of toss?)
 # - change long/lat accordingly based on our direction . newlat, newlong. write to file
 
-# Length in meters of 1° of latitude = always 111.32 km
-# Length in meters of 1° of longitude = 40075 km * cos( latitude ) / 360
+# Length in meters of 1 of latitude = always 111.32 km
+# Length in meters of 1 of longitude = 40075 km * cos( latitude ) / 360
 
 # but what is our 40.847671, 14.052652 format = fraction of 60 degrees
 
@@ -17,3 +17,58 @@
 # more accurate 1m= 0.00008983111*1.6666667= 0.00001497185
 
 # for long - calculate one degree then divide and re-mult
+
+# ///// read csv first value, open outfile
+
+# todo: test this
+
+from pylab import *
+import csv, os
+
+def read_csv_file(filename):
+    data = []
+    for row in csv.reader(open(filename)):
+        data.append(row)
+    return data
+
+heading=0 # NESw 0123
+filet="/root/notes_and_projectsNOW/oracles/20190614/112521.TXT"
+data=read_csv_file(filet)
+outfile=filet+".walked"
+oo=open(outfile,"w")
+
+runninglat=float(data[0][0])
+runninglong=float(data[0][1])
+
+oo.write(str(runninglat) + ", " + str(runninglong) + "\n")
+
+for rows in data:
+    # deal with our RNG stuff
+    rngg=float(rows[2])
+    dir=100-rngg
+    if dir>0:
+        heading+=1
+    else:
+        heading-=1
+    if heading==4:
+        heading=0
+    if heading==-1:
+        heading=3
+    metres=abs(rngg)
+    #    print metres, heading
+    # calc lat, long based on heading
+    if heading==0: # North so subtract from lat
+        runninglat-=metres*0.00001497185
+    if heading==2: # South so add to lat
+        runninglat+=metres*0.00001497185
+    if heading==1: # East so add to long
+        onedeginmeters=40075000 * cos( runninglat ) / 360
+        soonemeter=1/onedeginmeters
+        runninglong+=soonemeter*metres*1.6666667
+    if heading==3: # West so subtract from long
+        onedeginmeters=40075000 * cos( runninglat ) / 360
+        soonemeter=1/onedeginmeters
+        runninglong-=soonemeter*metres*1.6666667
+    oo.write(str(runninglat) + ", " + str(runninglong) + "\n")
+        
+oo.close
