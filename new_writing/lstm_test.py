ddd9906d47328053ceed448c8fa162534b8e697f
root <root@xxxxx.monl>
2020-03-22 13:58:50 +0100

    for new writings

---
+++ new_writing/lstm_test.py
@@ -0,0 +1,119 @@
+# from https://stackabuse.com/text-generation-with-python-and-tensorflow-keras/
+
+import numpy
+import sys
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
+
+file = open("razz83stripped.txt").read()
+
+def tokenize_words(input):
+    # lowercase everything to standardize it
+    input = input.lower()
+
+    # instantiate the tokenizer
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(input)
+
+    # if the created token isn't in the stop words, make it part of "filtered"
+    filtered = filter(lambda token: token not in stopwords.words('english'), tokens)
+    return " ".join(filtered)
+
+# preprocess the input data, make tokens
+processed_inputs = tokenize_words(file)
+
+chars = sorted(list(set(processed_inputs)))
+char_to_num = dict((c, i) for i, c in enumerate(chars))
+
+input_len = len(processed_inputs)
+vocab_len = len(chars)
+print ("Total number of characters:", input_len)
+print ("Total vocab:", vocab_len)
+
+#print(processed_inputs)
+
+seq_length = 100
+x_data = []
+y_data = []
+
+# loop through inputs, start at the beginning and go until we hit
+# the final character we can create a sequence out of
+for i in range(0, input_len - seq_length, 1):
+    # Define input and output sequences
+    # Input is the current character plus desired sequence length
+    in_seq = processed_inputs[i:i + seq_length]
+
+    # Out sequence is the initial character plus total sequence length
+    out_seq = processed_inputs[i + seq_length]
+
+    # We now convert list of characters to integers based on
+    # previously and add the values to our lists
+    x_data.append([char_to_num[char] for char in in_seq])
+    y_data.append(char_to_num[out_seq])
+
+n_patterns = len(x_data)
+print ("Total Patterns:", n_patterns)
+
+X = numpy.reshape(x_data, (n_patterns, seq_length, 1))
+X = X/float(vocab_len)
+
+# We'll now one-hot encode our label data:
+
+y = np_utils.to_categorical(y_data)
+
+model = Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(256, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(128))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+
+# We compile the model now, and it is ready for training:
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# It takes the model quite a while to train, and for this reason we'll save the weights and reload them when the training is finished. We'll set a checkpoint to save the weights to, and then make them the callbacks for our future model.
+
+filepath = "model_weights_saved.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+desired_callbacks = [checkpoint]
+
+# Now we'll fit the model and let it train.
+
+model.fit(X, y, epochs=4, batch_size=256, callbacks=desired_callbacks)
+
+# After it has finished training, we'll specify the file name and load in the weights. Then recompile our model with the saved weights:
+
+filename = "model_weights_saved.hdf5"
+model.load_weights(filename)
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# Since we converted the characters to numbers earlier, we need to define a dictionary variable that will convert the output of the model back into numbers:
+
+num_to_char = dict((i, c) for i, c in enumerate(chars))
+
+# To generate characters, we need to provide our trained model with a random seed character that it can generate a sequence of characters from:
+
+start = numpy.random.randint(0, len(x_data) - 1)
+pattern = x_data[start]
+print("Random Seed:")
+print("\"", ''.join([num_to_char[value] for value in pattern]), "\"")
+
+for i in range(1000):
+    x = numpy.reshape(pattern, (1, len(pattern), 1))
+    x = x / float(vocab_len)
+    prediction = model.predict(x, verbose=0)
+    index = numpy.argmax(prediction)
+    result = num_to_char[index]
+    seq_in = [num_to_char[value] for value in pattern]
+
+    sys.stdout.write(result)
+
+    pattern.append(index)
+    pattern = pattern[1:len(pattern)]
